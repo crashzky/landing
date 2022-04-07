abed9f71d0f90accbadad857117663bd7333f9a1
Kirill Osintsev <osintsev.2000@inbox.ru>
2022-04-07 20:07:33 +0500

    added animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from 'next/app';
+
 import '../styles/globals.css';
 import '../styles/font.css';
+import '../styles/animation.css';
+
+import 'aos/dist/aos.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
 	return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,41 @@ import Messengers from '../components/Messengers';
 import ProjectCard from '../components/ProjectCard';
 import Image from 'next/image';
 import PROJECTS from '../shared/consts/projects';
+import { useRouter } from 'next/router';
+import AOS from 'aos';
+import { useEffect } from 'react';
 
 import ArrowIcon from '../assets/arrow.svg';
-import { useRouter } from 'next/router';
 
 const MainPage = (): JSX.Element => {
 	const router = useRouter();
 
+	useEffect(() => {
+		AOS.init({
+			duration: 1000,
+		});
+	}, []);
+
 	return (
 		<main>
-			<Header />
-			<h1 className='text-[26px] font-bold text-center mt-3'>
-				Кирилл Осинцев
-			</h1>
-			<p className='text-center font-semibold text-gray mt-[10px]'>
-				Middle Front-End разработчик
-			</p>
-			<Messengers className='sm:w-[400px] mx-auto' />
-			<ArrowIcon className='mx-auto mt-14' />
+			<div data-aos='fade-zoom-in'>
+				<Header />
+				<h1 className='text-[26px] font-bold text-center mt-3'>
+					Кирилл Осинцев
+				</h1>
+				<p className='text-center font-semibold text-gray mt-[10px]'>
+					Middle Front-End разработчик
+				</p>
+				<Messengers className='sm:w-[400px] mx-auto' />
+			</div>
+			<ArrowIcon className='arrow mx-auto mt-14' />
 			<section className='grid gap-16 px-4 lg:px-[100px] xl:px-[200px]'>
 				<h2 className='mt-11 font-extrabold text-[50px] font-tt-travels'>
 					Мои проекты
 				</h2>
 				{PROJECTS.map((i, num) => (
 					<ProjectCard
+						data-aos={num % 2 === 0 ? 'fade-left' : 'fade-right'}
 						key={num}
 						imageSrc={i.imageSrc}
 						title={i.title}
@@ -35,7 +46,7 @@ const MainPage = (): JSX.Element => {
 						imagePosition={num % 2 === 0 ? 'left' : 'right'} />
 				))}
 			</section>
-			<Messengers className='sm:w-[400px] mx-auto mt-20 mb-32' />
+			<Messengers data-aos='fade-up' className='sm:w-[400px] mx-auto mt-20 mb-32' />
 			<button
 				className='mx-auto block w-fit mb-16'
 				onClick={() => router.push('https://wooble.space')}
